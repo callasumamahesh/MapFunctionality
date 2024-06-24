de4a21fef531d12c676779017d8b4618ff74a34a
callasumamahesh <bandaumamahesh1919@gmail.com>
2024-06-24 23:05:35 +0530

    last commit2

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,7 +1,8 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from 'firebase/auth'
-import { getAnalytics } from 'firebase/analytics'
+import { getAuth } from 'firebase/auth';
+import { getAnalytics } from 'firebase/analytics';
+
 // Use environment variables for Firebase config
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -10,14 +11,17 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
+// Initialize Firebase app
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const auth = getAuth()
+
+// Initialize Firebase Authentication and Firestore
+const auth = getAuth();
 const db = getFirestore(app);
 
-const analytics = getAnalytics(app)
+// Initialize Firebase Analytics (only on the client side)
+const analytics = app.name && typeof window !== 'undefined' ? getAnalytics(app) : null;
 
-export { auth,analytics, db };
+export { auth, analytics, db };
